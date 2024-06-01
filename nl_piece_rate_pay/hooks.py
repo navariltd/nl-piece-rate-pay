ce41d347ff492c3489bd77702abb5f3f422cc85c
Muchai <1822868+muchai@users.noreply.github.com>
2024-06-01 19:35:03 +0300

    Update hooks.py

--- nl_piece_rate_pay/hooks.py
+++ nl_piece_rate_pay/hooks.py
@@ -1,6 +1,6 @@
 app_name = "nl_piece_rate_pay"
 app_title = "Navari Piece Rate Pay"
-app_publisher = "Navari-Ltd"
+app_publisher = "Navari Ltd"
 app_description = "FrappeHR App for periodic payouts for piece rate payroll by navari Ltd"
 app_email = "mania@navari.co.ke"
 app_license = "GNU Affero General Public License v3.0"
